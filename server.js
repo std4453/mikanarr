fd9063888b2d52b71ec4a6e378b1f766c90a8e58
std4453 <me@std4453.com>
2021-04-21 11:55:11 +0800

    Bug fix in server

--- server.js
+++ server.js
@@ -1,55 +1,62 @@
 const config = {
-    port: 12306,
-    host: '127.0.0.1',
+  port: 12306,
+  host: "127.0.0.1",
 };
 
-const xml2js = require('xml2js');
+const xml2js = require("xml2js");
 const parser = new xml2js.Parser();
 const builder = new xml2js.Builder();
-const axios = require('axios');
+const axios = require("axios");
 const route = async (req, res) => {
+  try {
     const { data: xmlStr } = await axios.get(`https://mikanani.me/RSS/Classic`);
     const result = await parser.parseStringPromise(xmlStr);
 
     // pre-compile
-    const { data: database } = await axios.get(`http://${config.host}:${config.port}/patterns`);
+    const { data: database } = await axios.get(
+      `http://${config.host}:${config.port}/patterns`
+    );
     const rules = database.map(({ pattern, ...rest }) => ({
-        pattern: new RegExp(pattern, 'g'),
-        ...rest,
+      pattern: new RegExp(pattern, "g"),
+      ...rest,
     }));
 
     const items = [];
     for (const item of result.rss.channel[0].item) {
-        const {
-            title: [title],
-            enclosure,
-            link,
-            torrent: [{ pubDate }],
-        } = item;
-        for (const { pattern, series, season } of rules) {
-            const match = pattern.exec(title);
-            if (!match) continue;
-            const { episode } = match.groups;
-            const normalized = `${series} - S${season}E${episode}`;
-            items.push({
-                title: [normalized],
-                pubDate,
-                enclosure,
-                link,
-            });
-            break;
-        }
+      const {
+        title: [title],
+        enclosure,
+        link,
+        torrent: [{ pubDate }],
+      } = item;
+      for (const { pattern, series, season } of rules) {
+        const match = pattern.exec(title);
+        if (!match) continue;
+        const { episode } = match.groups;
+        const normalized = `${series} - S${season}E${episode}`;
+        items.push({
+          title: [normalized],
+          pubDate,
+          enclosure,
+          link,
+        });
+        break;
+      }
     }
     result.rss.channel[0].item = items;
     res.set("Content-Type", "text/xml");
     res.send(builder.buildObject(result));
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Internal Server Error");
+  }
 };
 
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("data/database.json");
 const middlewares = jsonServer.defaults({
-  static: './build',
+  static: "./build",
 });
 
 server.use(middlewares);
