424ac5ac783b58e62efbb3dd6de1f24a19c40d67
std4453 <me@std4453.com>
2021-04-21 13:38:48 +0800

    Split frontend code to multiple files

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import jsonServerProvider from "ra-data-json-server";
+import React from "react";
+import { Admin, Datagrid, List, Resource, TextField } from "react-admin";
+import { PatternCreate, PatternEdit } from "./Edit";
+
+const dataProvider = jsonServerProvider(new URL("/api", location.href).href);
+
+const PatternList = (props) => (
+  <List {...props}>
+    <Datagrid rowClick="edit">
+      <TextField source="id" />
+      <TextField source="pattern" />
+      <TextField source="series" />
+      <TextField source="season" />
+    </Datagrid>
+  </List>
+);
+
+const App = () => (
+  <React.StrictMode>
+    <Admin dataProvider={dataProvider}>
+      <Resource
+        name="patterns"
+        list={PatternList}
+        edit={PatternEdit}
+        create={PatternCreate}
+      />
+    </Admin>
+  </React.StrictMode>
+);
+
+export default App;
---
+++ src/Edit.jsx
@@ -0,0 +1,88 @@
+import { Button, InputAdornment } from "@material-ui/core";
+import axios from "axios";
+import * as _ from "lodash";
+import React from "react";
+import {
+  AutocompleteInput,
+  Create,
+  Edit,
+  SimpleForm,
+  TextInput,
+  useNotify,
+} from "react-admin";
+import { useForm, useFormState } from "react-final-form";
+import useSWR from "swr";
+
+const EscapeButton = () => {
+  const form = useForm();
+  const formState = useFormState();
+  const escape = () =>
+    form.change("pattern", _.escapeRegExp(formState.values.pattern));
+  return (
+    <InputAdornment position="end">
+      <Button color="primary" onClick={escape}>
+        Escape
+      </Button>
+    </InputAdornment>
+  );
+};
+
+const choicesFetcher = async (api) => {
+  const { data } = await axios(
+    `${process.env.REACT_APP_SONARR_API_ROOT}${api}?apikey=${process.env.REACT_APP_SONARR_API_KEY}`
+  );
+  return data.map(({ title }) => ({ id: title, name: title }));
+};
+
+const useSeriesChoices = () => {
+  const notify = useNotify();
+  const { data: choices } = useSWR("/series", choicesFetcher, {
+    onError: (err) => {
+      console.error(e);
+      notify(`Fetch Sonarr series failed: ${e.message}`);
+    },
+  });
+  return choices;
+};
+
+const PatternEdit = (props) => {
+  const choices = useSeriesChoices();
+  return (
+    <Edit {...props}>
+      <SimpleForm>
+        <TextInput disabled source="id" />
+        <TextInput
+          fullWidth
+          source="pattern"
+          InputProps={{
+            endAdornment: <EscapeButton />,
+          }}
+        />
+        <AutocompleteInput fullWidth source="series" choices={choices} />
+        <TextInput source="season" />
+      </SimpleForm>
+    </Edit>
+  );
+};
+
+const PatternCreate = (props) => {
+  const choices = useSeriesChoices();
+  return (
+    <Create {...props}>
+      <SimpleForm>
+        <TextInput disabled source="id" />
+        <TextInput
+          fullWidth
+          source="pattern"
+          InputProps={{
+            endAdornment: <EscapeButton />,
+          }}
+        />
+        <AutocompleteInput fullWidth source="series" choices={choices} />
+        <TextInput source="season" />
+      </SimpleForm>
+    </Create>
+  );
+};
+
+export { PatternCreate, PatternEdit };
