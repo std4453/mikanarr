89841aa04ffdf6871554e4ee9360b0acb2b6e947
std4453 <me@std4453.com>
2021-09-12 13:34:18 +0800

    Sort items by id desc on default & search

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import jsonServerProvider from "ra-data-json-server";
 import React from "react";
-import { Admin, Datagrid, List, Resource, TextField } from "react-admin";
+import {
+  Admin,
+  Datagrid,
+  List,
+  Resource,
+  TextField,
+  TextInput,
+} from "react-admin";
 import { SWRConfig } from "swr";
 import { PatternCreate, PatternEdit } from "./Edit";
 
 const dataProvider = jsonServerProvider(new URL("/api", location.href).href);
 
+const patternFilters = [<TextInput label="Search" source="q" alwaysOn />];
+
 const PatternList = (props) => (
-  <List {...props}>
+  <List
+    sort={{
+      field: "id",
+      order: "DESC",
+    }}
+    filters={patternFilters}
+    {...props}
+  >
     <Datagrid rowClick="edit">
       <TextField source="id" />
       <TextField source="pattern" />
